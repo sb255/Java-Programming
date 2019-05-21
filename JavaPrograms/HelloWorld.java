b3e797ece13e17eb8fadaa0ed865988c1f2e2698
sb255 <samarthbais1@gmail.com>
2019-05-21 14:55:14 +0530

    running our first class

---
+++ JavaPrograms/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class HelloWorld {
+
+  public static void main(String[] args){
+  
+    System.out.println("Hello, this is my first program through terminal");
+  
+  }
+
+}
