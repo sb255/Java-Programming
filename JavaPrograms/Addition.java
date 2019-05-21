bc5b4e9bb506250e51a1de71e82ef8f096b66e40
sb255 <samarthbais1@gmail.com>
2019-05-21 16:16:34 +0530

    making an simple addition progrma

---
+++ JavaPrograms/Addition.java
@@ -0,0 +1,20 @@
+import java.lang.*; 
+
+public class Addition{
+
+int a,b;
+
+/*Making it static, means it will be called on the compile time*/
+public static int addition(int a,int b){
+
+  return (a+b);
+ 
+  }
+
+public static void main(String args[]){
+
+  System.out.println(addition(5,5));
+  
+
+  }
+}
